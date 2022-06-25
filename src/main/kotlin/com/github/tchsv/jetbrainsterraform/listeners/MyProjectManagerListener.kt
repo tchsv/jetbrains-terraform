45fef47089b2567a5b56cf1e821e752b3dc774cf
GitHub Action <action@github.com>
2022-06-25 18:09:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/tchsv/jetbrainsterraform/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tchsv/jetbrainsterraform/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tchsv.jetbrainsterraform.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tchsv.jetbrainsterraform.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/tchsv/jetbrainsterraform/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tchsv/jetbrainsterraform/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tchsv.jetbrainsterraform.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tchsv.jetbrainsterraform.MyBundle
 
 class MyProjectService(project: Project) {
 
